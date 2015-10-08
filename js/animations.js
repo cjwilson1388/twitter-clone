b55ab6b28fc1fb9658e32895e32362da0e135c5a
CJ Wilson <cjwilson1388@gmail.com>
2015-10-07 21:48:18 -0600

    end of class. twitter word counter working on turning text red

--- js/animations.js
+++ js/animations.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
 	$('#tweet-controls').css({
 		display: 'none'
@@ -6,15 +7,36 @@ $(document).ready(function(){
 		$('#tweet-controls').css({
 			display: 'inherit'
 		})
-		$('#mainCompose').css("height", '5em')
+		$('#mainCompose').css("height", '6em')
 	});
-
-
+	$("#mainCompose").keyup(function(event) {
+		tc=$(this);
+		if(tc.val().length >= 140){
+			tc.val( tc.val().substr(0, 140) );
+		} else {
+			$('#char-count').text(140 - tc.val().length);
+			}
+		});
 
 });
+
+
+
+
+
+
+
 
 /*
-$( "#target" ).click(function() {
-  alert( "Handler for .click() called." );
+$('#mainCompose').keyup(function(event) {
+	tc=$(this);
+	if(tc.val().length >= 140){
+		tc.val( tc.val().substr(0, 140) );
+	} else {
+		$('#char-count').text(140 - tc.val().length);
+		}
+	}
 });
 */
+
+//.text();
